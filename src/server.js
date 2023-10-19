0660bd295d6e82bd36206dd86233f489e8522e74
Leonardo S Matos <leonardo.matos@unimednatal.com.br>
2023-10-19 16:04:51 -0300

    Corpo da requisição (Stream e buffers)

--- src/server.js
+++ src/server.js
@@ -2,8 +2,21 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  const buffers = []
+
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/users') {
     return res
@@ -12,14 +25,15 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
 
     users.push({
       id: 1,
-      name: 'Jonh Doe',
-      email: 'jonhdoe@example.com'
+      name,
+      email
     })
 
-    return res.writeHead(201).end('Criação de usuário')
+    return res.writeHead(201).end()
   }
 
   return res.writeHead(404).end()
